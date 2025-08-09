3f8c1497fdcf9874afab59bd47a65bbaa380fa3e
mdhilal9153 <mdhilal123098@gmail.com>
2025-08-09 16:30:12 +0530

    home

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+
+
+app.get("/",(req,res) => {
+    res.render("home.ejs");
+})
+
+
+app.listen(8080,() => {
+    console.log("server is listening on port 8080...");
+})
