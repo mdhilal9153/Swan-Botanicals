28de7368d77b4494d308a66e8ff2183b70b5c51c
mdhilal9153 <mdhilal123098@gmail.com>
2025-08-10 18:57:50 +0530

    Updated

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsMate = require("ejs-mate");
 
 
+app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 
 
-app.get("/",(req,res) => {
+app.get("/home",(req,res) => {
     res.render("home.ejs");
 })
 
+app.get("/ourstory",(req,res) => {
+    res.render("ourstory.ejs");
+})
 
 app.listen(8080,() => {
     console.log("server is listening on port 8080...");
